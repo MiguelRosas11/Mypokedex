1259ad8f27637269d360e4ac6e3a286cdfce8228
MiguelRosas11 <roguemijose@gmail.com>
2025-08-02 21:35:10 -0600

    Parte 4 terminada

--- app/src/main/java/com/example/mypokedex/data/model/Pokemon.kt
+++ app/src/main/java/com/example/mypokedex/data/model/Pokemon.kt
@@ -1,7 +1,29 @@
 package com.example.mypokedex.data.model
 
+
+enum class PokemonType{
+    WATER,
+    FIRE,
+    GRASS,
+    ELECTRIC,
+    ROCK;
+}
+
+
 data class Pokemon(
     var id: Int = 0,
     val nombre: String,
-    val
-)
+    val maxHP: Int,
+    val type: PokemonType
+){
+    companion object{
+        private var idCounter = 0
+
+        fun CreatePokemon(nombre: String, maxHP: Int, type: PokemonType): Pokemon {
+            idCounter++
+            return Pokemon(idCounter, nombre, maxHP, type)
+        }
+    }
+
+}
+
